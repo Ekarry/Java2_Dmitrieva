cfd9432821a2f58f4a6961e71a70e115946e7352
Ekarry <81385639+Ekarry@users.noreply.github.com>
2021-05-28 20:37:32 +0300

    Прошу проверить ДЗ4

---
+++ src/ru/gb/Dmitrieva/HomeWorkApp/Lesson_4/Main.java
@@ -0,0 +1,60 @@
+package ru.gb.Dmitrieva.HomeWorkApp.Lesson_4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Задание 1
+        System.out.println("Считаем, сколько раз встречается каждое слово:");
+        countArray();
+
+        //Задание 2
+        System.out.println("Телефонная книга. Добавляем и ищем по фамилии:");
+        Phonebook phonebook = new Phonebook();
+
+        phonebook.putInfo("Popov", "+79999999991");
+        phonebook.putInfo("Svetlov", "+79999999992");
+        phonebook.putInfo("Kononenko", "+79999999993");
+        phonebook.putInfo("Popov", "+7911111111111");
+        phonebook.putInfo("Chernichev", "+791111222222");
+        phonebook.putInfo("Popov", "+7922222222");
+
+        phonebook.getInfo("Popov");
+        phonebook.getInfo("Chernichev");
+    }
+    private static void countArray() {
+        ArrayList<String> array = new ArrayList<>();
+        array.add("tea");
+        array.add("king");
+        array.add("map");
+        array.add("desert");
+        array.add("tea");
+        array.add("tea");
+        array.add("tea");
+        array.add("monkey");
+        array.add("eagle");
+        array.add("desert");
+        array.add("monkey");
+        array.add("map");
+        array.add("tea");
+        array.add("tea");
+        array.add("king");
+        array.add("map");
+        array.add("tea");
+        array.add("tea");
+        array.add("monkey");
+        array.add("monkey");
+
+        HashSet<String> arrayUnique = new HashSet<>(array);
+        ArrayList<String> arrayCount = new ArrayList<>(arrayUnique);
+
+        for (int i = 0; i < arrayUnique.size(); i++) {
+            System.out.println("Количество слов \"" + arrayCount.get(i) + "\": " + Collections.frequency(array, arrayCount.get(i)));
+        }
+        System.out.println("\n");
+    }
+
+}
